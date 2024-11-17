56396a844f407c531f8b48486a17c7c55903090b
atom <sthaejan00@gmail.com>
2024-11-17 21:23:05 +0545

    :sparkles: added cachce generic repository

---
+++ JobCandidate.Domain/Interfaces/ICacheRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace JobCandidate.Domain.Interfaces
+{
+    public interface ICacheRepository<T> where T : class
+    {
+        T Get(string key);
+        void Set(string key, T item);
+        void Remove(string key);
+    }
+
+}
---
+++ JobCandidate.Infrastructure/Caching/CacheRepository.cs
@@ -0,0 +1,34 @@
+﻿using JobCandidate.Domain.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace JobCandidate.Infrastructure.Caching
+{
+    public class CacheRepository<T> : ICacheRepository<T> where T : class
+    {
+        private readonly IMemoryCache _cache;
+        private readonly TimeSpan _cacheDuration;
+
+        public CacheRepository(IMemoryCache cache, TimeSpan? cacheDuration = null)
+        {
+            _cache = cache;
+            _cacheDuration = cacheDuration ?? TimeSpan.FromMinutes(5); // Default to 5 minutes
+        }
+
+        public T Get(string key)
+        {
+            _cache.TryGetValue(key, out T item);
+            return item;
+        }
+
+        public void Set(string key, T item)
+        {
+            _cache.Set(key, item, _cacheDuration);
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+    }
+
+}
