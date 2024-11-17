08635a7c0302bd6f55cedeaabb70008dd2908a2c
atom <sthaejan00@gmail.com>
2024-11-17 21:25:49 +0545

    :sparkles: added candidatedb context

---
+++ JobCandidate.Infrastructure/Persistence/CandidateDbContext.cs
@@ -0,0 +1,24 @@
+﻿using JobCandidate.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobCandidate.Infrastructure.Persistence
+{
+    public class CandidateDbContext : DbContext
+    {
+        public DbSet<Candidate> Candidates { get; set; }
+
+        public CandidateDbContext(DbContextOptions<CandidateDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Candidate>(entity =>
+                {
+                    entity.HasKey(c => c.Id);
+                    entity.Property(c => c.Email).IsRequired().HasMaxLength(100);
+                    entity.HasIndex(c => c.Email).IsUnique(true);
+                });
+        }
+    }
+
+}
