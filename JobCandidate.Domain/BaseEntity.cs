7217be0ee8295a9b0b925e27370fdb1a2603befe
atom <sthaejan00@gmail.com>
2024-11-17 16:30:07 +0545

    Add project files.

---
+++ JobCandidate.Domain/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidate.Domain
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ JobCandidate.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace JobCandidate.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
