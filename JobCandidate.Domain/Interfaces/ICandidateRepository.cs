3039f34e2eace422ac41ee288f48bdd5c2170209
atom <sthaejan00@gmail.com>
2024-11-17 21:27:31 +0545

    :sparkles: added CandidateRepository

---
+++ JobCandidate.Domain/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,17 @@
+﻿using JobCandidate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobCandidate.Domain.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<Candidate> GetByEmailAsync(string email);
+        Task AddAsync(Candidate candidate);
+        Task UpdateAsync(Candidate candidate);
+    }
+
+}
---
+++ JobCandidate.Infrastructure/Repositories/CandidateRepository.cs
@@ -0,0 +1,36 @@
+﻿using JobCandidate.Domain.Entities;
+using JobCandidate.Domain.Interfaces;
+using JobCandidate.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobCandidate.Infrastructure.Repositories
+{
+    public class CandidateRepository : ICandidateRepository
+    {
+        private readonly CandidateDbContext _context;
+
+        public CandidateRepository(CandidateDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Candidate> GetByEmailAsync(string email)
+        {
+            var candidate = await _context.Candidates.FirstOrDefaultAsync(c => c.Email == email);
+            return candidate;
+        }
+
+        public async Task AddAsync(Candidate candidate)
+        {
+            _context.Candidates.Add(candidate);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Candidate candidate)
+        {
+            candidate.UpdatedOn = DateTime.UtcNow;
+            _context.Candidates.Update(candidate);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
