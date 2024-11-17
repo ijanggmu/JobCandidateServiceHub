54320099887de0191e3fd11e602f236b2b0acfc9
atom <sthaejan00@gmail.com>
2024-11-17 16:35:44 +0545

    :sparkles: added candidate entity

--- JobCandidate.Domain/Entities/Candidate.cs
+++ JobCandidate.Domain/Entities/Candidate.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JobCandidate.Domain.Entities
+﻿namespace JobCandidate.Domain.Entities
 {
-    internal class Candidate:BaseEntity
+    public class Candidate : BaseEntity
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CallInterval { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string GitHubUrl { get; set; }
+        public string Comment { get; set; }
     }
 }
